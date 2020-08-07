78124cfd2f244df798a1cc3d8d1deda623188875
divesh9205 <divesh9205@gmail.com>
2020-08-07 14:10:38 +0530

    social links added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img alt="logo" src="https://firebasestorage.googleapis.com/v0/b/portfolio-991b9.appspot.com/o/under_construction.gif?alt=media&token=ab184eaa-1e76-4827-8288-04d93f3214bd"/>
-        <h4>Under Construction</h4>
-        <p>
-
-          Divesh Singh portfolio
-        </p>
+      <p>Divesh Singh portfolio</p>
+      <img alt="Under construction" src="https://firebasestorage.googleapis.com/v0/b/portfolio-991b9.appspot.com/o/under_construction.gif?alt=media&token=ab184eaa-1e76-4827-8288-04d93f3214bd"/>
+      <img alt="construction" src="https://firebasestorage.googleapis.com/v0/b/portfolio-991b9.appspot.com/o/construction.webp?alt=media&token=9337fcc4-9b5f-4d8a-8ec3-ced0388e9b85"/>
+        <p>Meanwhile you can contact me at</p><p className="email">contact@diveshsingh.in</p>
+        <div className="social">
+          <p>OR visit me at</p>
+          <button><a href="https://github.com/Divesh-singh">GitHub</a></button>{' '}
+          <button><a href="https://www.instagram.com/divesh.js/">Instagram</a></button>{' '}
+          <button><a href="https://www.linkedin.com/in/divesh-kumar-singh-222827167/">Linkedin</a></button>{' '}
+          <button><a href="https://twitter.com/divesh_js">Twitter</a></button>
+        </div>
       </header>
     </div>
   );
